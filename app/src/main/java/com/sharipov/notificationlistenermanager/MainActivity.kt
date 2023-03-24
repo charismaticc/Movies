b59e172715a1d9ab60af21ac3c521a7ed8031752
Charizmatik <akbar20sharipov01@gmail.com>
2023-03-24 17:52:07 +0300

    Add:  + NavHostController  + Screens (Mans, Splash, Details)  + Splash Screen

--- app/src/main/java/com/sharipov/notificationlistenermanager/MainActivity.kt
+++ app/src/main/java/com/sharipov/notificationlistenermanager/MainActivity.kt
@@ -3,11 +3,8 @@ package com.sharipov.notificationlistenermanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.sharipov.notificationlistenermanager.navigation.SetupNavHost
 import com.sharipov.notificationlistenermanager.ui.theme.NotificationListenerManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,12 +13,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotificationListenerManagerTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Text(text = "Hello!")
-                }
+                val navController = rememberNavController()
+                SetupNavHost(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/com/sharipov/notificationlistenermanager/screens/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.sharipov.notificationlistenermanager.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Text(text = "Main Screen")
+}
